2b11f4f42edd4d3aeef6f4c004b597926715a238
kinnixx1 <focsasveta@gmail.com>
2023-11-09 19:36:42 +0200

    hy

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Hi, I am an another JS");
+console.log("Message in the console")
+
+let var1 = 1;
+let var2 = "something";
+let var3 = 45.4;
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
